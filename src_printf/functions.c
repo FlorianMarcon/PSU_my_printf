c76bd9e972303db9ddb2a4a6bc9626eb871f0c31
marcon <florian.marcon@epitech.eu>
2017-11-15 17:26:34 +0100

    resolution hexa low case

--- src_printf/functions.c
+++ src_printf/functions.c
@@ -130,7 +130,7 @@ int	printHexa(va_list tmp, char *str)
 
 	string = convert_base(string, "0123456789", "0123456789abcdef");
 	len += speci_printHexa(str, string);
-	string = my_strlowcase(str);
+	string = my_strlowcase(string);
 	my_putstr(string);
 	return (len + 1);
 }
